43548a11be7c9c27874388b1a2bd60abeb008f40
Gustavo Silva <gustavopereirasilva376@gmail.com>
2026-01-24 15:22:38 -0300

    Update exercicio 1.py

--- Python Basico/Exercicios/exercicio 1.py
+++ Python Basico/Exercicios/exercicio 1.py
@@ -1,6 +1,41 @@
 print("hello word")
 
+#Duas mensagens
 print("Aprendendo Python")
 print("Primeira aula")
 
-print("")
+#Ordem de mensagens
+print("Primeira mensagem")
+print("Segunda mensagem")
+
+#Mensagem de Error a nao ser que coloque ponto e virgula
+print("mensagem erro"); print("mensagem erro")
+
+#definindo variavel
+nome = "Gustavo"
+print(f"Meu nome é {nome}")
+
+nome = input("Insira seu nome: ")
+print(f"Seu nome é {nome}")
+
+#soma de variaveis
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+# resultado = a + b
+print(f"A soma de {a} + {b} é: {a + b}")
+
+
+#erro de variavel nao definida
+a = 1
+b = 2
+c = d + 1
+print(c)
+
+#usando type
+a = "Olá mundo"
+b = 3.14
+c = 10
+print(type(a))
+print(type(b))
+print(type(c))
+
